f31fedc7dbbf63e0ddadefd69a6cf74e2e581caf
jess <jcottrill1123@gmail.com>
2022-08-15 17:38:00 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MethodOverloadingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Methods.Add(4, 7));
+            Console.WriteLine(Methods.Add(4.6m, 6.7m));
+            Console.WriteLine(Methods.Add(5, 9, true));
+            Console.WriteLine(Methods.Add(0, 1, true));
+            Console.WriteLine(Methods.Add(0,1, false));
+        }
+    }
+}
